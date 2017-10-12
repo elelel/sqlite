22b36a47e782ec7a083a54eb7b4b54ec53ab6dc5
El El <elel@3wh.net>
2017-10-12 14:03:25 +0000

    Clean TDD test

--- test/src/sqlite_test.cpp
+++ test/src/sqlite_test.cpp
@@ -41,20 +41,21 @@ SCENARIO("Taste") {
   }
 }
 
-SCENARIO("TDD vertical") {
+SCENARIO("TDD") {
   namespace sqlite = elelel::sqlite;
   unlink("test.sqlite");
   auto db = std::make_shared<sqlite::database>("test.sqlite");
-  sqlite::query q(db, R"(
+  sqlite::query create(db, R"(
 CREATE TABLE `table1` (
-  `int_field` INTEGER,
-  `int_field_not_null` INTEGER NOT NULL,
-  `int64` INTEGER
+  `int` INTEGER,
+  `int_not_null` INTEGER NOT NULL,
+  `int64` INTEGER,
+  `int64_not_null` INTEGER NOT NULL
 );
 )");
-  q.execute();
+  create.execute();
   WHEN("Binding single argument") {
-    sqlite::query<sqlite::params<int32_t>::type> q(db, "SELECT count(*) FROM `table1` WHERE `int_field` = ?");
+    sqlite::query<sqlite::params<int32_t>::type> q(db, "SELECT count(*) FROM `table1` WHERE `int` = ?");
     WHEN("Using bind_value with index") {
       q.params.bind_wrapped_value(1, std::optional<int32_t>(1));
     }
@@ -73,7 +74,7 @@ CREATE TABLE `table1` (
   }
   WHEN("Binding two arguments") {
     using params_type = sqlite::params<int32_t, int32_t>::type;
-    sqlite::query<params_type> q(db, "SELECT count(*) FROM `table1` WHERE `int_field` = ? and `int_field_not_null` = ?");
+    sqlite::query<params_type> q(db, "SELECT count(*) FROM `table1` WHERE `int` = ? and `int_not_null` = ?");
     WHEN("Using bind_values variadic") {
       q.params.bind_wrapped_values(std::optional<int32_t>(1), std::optional<int32_t>(2));
     }
@@ -88,58 +89,56 @@ CREATE TABLE `table1` (
       q.params.bind(sqlite::make_params(i1, i2));
     }
   }
-  WHEN("Inserting a record") {
-    using params_type = sqlite::params<int32_t, int32_t>::type;
-    sqlite::query<params_type> q_insert(db, "INSERT INTO `table1` (`int_field`, `int_field_not_null`) VALUES (?, ?)");
-    WHEN("Binding parameters") {
-      WHEN("wrapped") {
-        WHEN("as params tuple") {
-          q_insert.params.bind(sqlite::make_params(1, 2));
-          q_insert.execute();
-          WHEN("Selecting single row") {
-            using params_type = sqlite::params<int32_t, int32_t>::type;
-            using results_type = sqlite::row<int32_t, int32_t>::type;
-            sqlite::query<params_type, results_type> q(db, "SELECT `int_field`, `int_field_not_null` FROM `table1`");
-            q.execute();
-            WHEN("Requesting single element") {
-              REQUIRE(q.results.get<int32_t>(0).value() == 1);
-              REQUIRE(q.results.get<int32_t>(1).value() == 2);
-            }
-            WHEN("Requesting row") {
-              results_type row = q.results.row();
-              REQUIRE(std::get<0>(row).value() == 1);
-              REQUIRE(std::get<1>(row).value() == 2);
-            }
-            WHEN("Inserting a second row and using row iterator") {
-              q_insert.params.clear();
-              q_insert.reset();
-              q_insert.params.bind(sqlite::make_params(3, 4));
-              q_insert.execute();
-          
-              sqlite::query<params_type, results_type> q(db, "SELECT `int_field`, `int_field_not_null` FROM `table1`");
-              size_t count{0};
-              for (const auto r : q.results) {
-                REQUIRE(*std::get<1>(r) - *std::get<0>(r) == 1);
-                ++count;
-              }
-              REQUIRE(count == 2);
-            }
-          }
-        }
+  WHEN("Create insert query") {
+    using params_type = sqlite::params<int32_t, int32_t, int64_t, int64_t>::type;
+    sqlite::query<params_type> ins(db, "INSERT INTO `table1` (`int`, `int_not_null`, `int64`, `int64_not_null`) VALUES (?, ?, ?, ?)");
+    WHEN("Bind insert parameters") {
+      WHEN("as named params tuple") {
+        auto params = sqlite::make_params(201, 202, 203, 204);
+        ins.params.bind(params);
       }
-    }
-    WHEN("unwrapped") {
-      WHEN("as lvalue params tuple") {
-        auto params = sqlite::make_params(1, 2);
-        q_insert.params.bind(params);
-      }
-      WHEN("Binding by index") {
+      WHEN("by index") {
         WHEN("One by one") {
-          q_insert.params.bind_value(1, 1);
-          q_insert.params.bind_value(1, 2);
+          ins.params.bind_value(1, 1);
+          ins.params.bind_value(1, 2);
         }
         WHEN("Variadic") {
-          q_insert.params.bind_values(1, 2);
+          ins.params.bind_values(1, 2);
+        }
+      }
+      WHEN("as params tuples; with execute()") {
+        ins.params.bind(sqlite::make_params(0x101, 0x102, 0x103, 0x104));
+        ins.execute();
+
+        ins.params.clear();
+        ins.reset();
+        ins.params.bind(sqlite::make_params(0x201, 0x202, 0x203, 0x204));
+        ins.execute();
+        THEN("Select the rows") {
+          using results_type = sqlite::row<int32_t, int32_t, int64_t, int64_t>::type;
+          sqlite::query<std::tuple<>, results_type> sel(db, "SELECT `int`, `int_not_null`, `int64`, `int64_not_null` FROM `table1`");
+          sel.execute();
+          // Requesting single element
+          REQUIRE((sel.results.get<int32_t>(0).value() & 0xf) == 1);
+          REQUIRE((sel.results.get<int32_t>(1).value() & 0xf) == 2);
+          REQUIRE((sel.results.get<int64_t>(2).value() & 0xf) == 3);
+          REQUIRE((sel.results.get<int64_t>(3).value() & 0xf) == 4);
+          // Requesting row
+          results_type row = sel.results.row();
+          REQUIRE((std::get<0>(row).value() & 0xf) == 1);
+          REQUIRE((std::get<1>(row).value() & 0xf) == 2);
+          REQUIRE((std::get<2>(row).value() & 0xf) == 3);
+          REQUIRE((std::get<3>(row).value() & 0xf) == 4);
+    
+          size_t count{0};
+          for (const auto r : sel.results) {
+            REQUIRE((std::get<0>(r).value() & 0xf) == 1);
+            REQUIRE((std::get<1>(r).value() & 0xf) == 2);
+            REQUIRE((std::get<2>(r).value() & 0xf) == 3);
+            REQUIRE((std::get<3>(r).value() & 0xf) == 4);
+            ++count;
+          }
+          REQUIRE(count == 2);
         }
       }
     }
